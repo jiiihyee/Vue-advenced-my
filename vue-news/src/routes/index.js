109b55f0a9f67940e0cf2e7e511487cc3e863d18
jihye_son <jiiihyeee98@gmail.com>
2022-05-18 11:25:40 +0900

    router 까지 완료

---
+++ vue-news/src/routes/index.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import NewsView from '../views/NewsView.vue'
+import JobsView from '../views/JobsView.vue'
+import AskView from '../views/AskView.vue'
+import UserView from '../views/UserView.vue'
+import ItemView from '../views/ItemView.vue'
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+  mode:'history',
+  routes:[
+    {
+        path:'/news',      //url 주소
+        component:NewsView, //url 주소로 갔을 때 표시된 컴포넌트 
+    },
+    {
+        path:'/ask',
+        component:AskView,
+    },
+    {
+        path:'/jobs',
+        component:JobsView,
+    },
+    {
+      path:'/user',
+      component:UserView,
+    },
+    {
+      path:'/item',
+      component:ItemView,
+    }
+  ]
+});
